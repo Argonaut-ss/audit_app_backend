3e800343616f51cd67bcb59041c59690306dff2a
Rynix <adrian.ananta05@gmail.com>
2026-08-10 14:15:06 +0700

    Remove unused imports from routes/api.php
    Removed unused DosenController and MahasiswaController imports that were not referenced in the routes file.

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Http\Controllers\DosenController;
-use App\Http\Controllers\MahasiswaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
