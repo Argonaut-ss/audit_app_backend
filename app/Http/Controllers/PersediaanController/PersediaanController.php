e558cb744d6b56325974142c8c3770025633b228
SoulCalmes <michaelhanjaya24@gmail.com>
2026-09-22 14:13:59 +0700

    Update PersediaanController.php

--- app/Http/Controllers/PersediaanController/PersediaanController.php
+++ app/Http/Controllers/PersediaanController/PersediaanController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\PersediaannController;
+namespace App\Http\Controllers\PersediaanController;
 
 use App\Http\Controllers\Controller;
 use App\Models\JwbKasus;
