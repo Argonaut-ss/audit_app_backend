8c3bdefc2669c37a83e31b871a7549d38ec0fb38
SoulCalmes <michaelhanjaya24@gmail.com>
2026-09-22 13:00:14 +0700

    Update RekapBalasanUtangUsahaController.php

--- app/Http/Controllers/UtangUsahaController/RekapBalasanUtangUsahaController.php
+++ app/Http/Controllers/UtangUsahaController/RekapBalasanUtangUsahaController.php
@@ -1028,7 +1028,7 @@ class RekapBalasanUtangUsahaController extends Controller
                         $utangUsaha->updateQuietly([
                             'RekapCheck' =>
                                 $utangUsaha
-                                    ->rekapBalasanUtangUsaha()
+                                    ->rekapBalasan()
                                     ->exists(),
                         ]);
                     }
@@ -1290,7 +1290,7 @@ class RekapBalasanUtangUsahaController extends Controller
             $utangUsaha->updateQuietly([
                 'RekapCheck' =>
                     $utangUsaha
-                        ->rekapBalasanUtangUsaha()
+                        ->rekapBalasan()
                         ->exists(),
             ]);
         }
